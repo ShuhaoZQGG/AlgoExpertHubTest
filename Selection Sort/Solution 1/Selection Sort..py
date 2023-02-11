475789f6e02d06ffc1e73b0cfc2da12f4887d963
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-02-10 22:12:19 -0500

    Create Python Solution 1 for question Selection Sort

---
+++ Selection Sort/Solution 1/Selection Sort..py
@@ -0,0 +1,15 @@
+def selectionSort(array):
+    # Write your code here.
+    i = 0
+    while i < len(array) - 1:
+         j, smallest = i, i
+         while j < len(array) - 1:
+             j += 1
+             if array[j] < array[smallest]:
+                 smallest = j
+         array[i], array[smallest] = array[smallest], array[i]
+         i += 1
+    return array
+        
+        
+
