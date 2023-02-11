2679aa3bfbbcd4dd5be5b169ec93b74d8fe4eabc
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-02-10 22:07:14 -0500

    Change Python Solution 3 for question Tournament Winner

---
+++ Tournament Winner/Solution 3/Tournament Winner..py
@@ -0,0 +1,23 @@
+def tournamentWinner(competitions, results):
+    # Write your code here.
+    hm = {}
+    result = ""
+    h = 0
+    for i in range(len(competitions)):
+        if competitions[i][0] not in hm:
+            hm[competitions[i][0]] = results[i]
+        else:
+            hm[competitions[i][0]] += results[i]
+
+
+        if competitions[i][1] not in hm:
+            hm[competitions[i][1]] = 1 - results[i]
+        else:
+            hm[competitions[i][1]] += 1 - results[i] 
+        if hm[competitions[i][0]] > h:
+            h = hm[competitions[i][0]] 
+            result = competitions[i][0]
+        if hm[competitions[i][1]] > h:
+            h = hm[competitions[i][1]] 
+            result = competitions[i][1]
+    return result
