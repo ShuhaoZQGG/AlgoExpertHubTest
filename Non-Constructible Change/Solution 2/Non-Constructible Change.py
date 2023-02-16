46ba56557ef04abbe2335652a6207e4290d867f9
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-02-16 16:08:37 -0500

    Change Python Solution 2 for question Non-Constructible Change

--- Non-Constructible Change/Solution 2/Non-Constructible Change.py
+++ Non-Constructible Change/Solution 2/Non-Constructible Change.py
@@ -1,25 +1,10 @@
 def nonConstructibleChange(coins):
     # Write your code here.
-    sortedCoins = sorted(coins)
-    i = 0
-    sum = 0
-    while i < len(sortedCoins):
-        if i == len(sortedCoins) - 1:
-            if sortedCoins[i] > sum + 1:
-                return sum + 1
-            else:
-                return sortedCoins[i] + sum + 1
-        sum += sortedCoins[i]
-
-
-        if sortedCoins[i + 1] > sum + 1:
-            return sum + 1
-
-
-
-
-        i += 1
-
-
-    return sum + 1
+    coins.sort()
+    minimum_change = 0
+    for coin in coins:
+        if coin > minimum_change + 1:
+            break
+        minimum_change += coin
+    return minimum_change + 1
 
