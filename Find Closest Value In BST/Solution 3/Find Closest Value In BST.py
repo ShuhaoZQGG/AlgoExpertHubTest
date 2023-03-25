3e9848e2be228705a806e2d917b06f280a66b6f6
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-03-25 17:16:43 -0400

    Change Python Solution 3 for question Find Closest Value In BST

--- Find Closest Value In BST/Solution 3/Find Closest Value In BST.py
+++ Find Closest Value In BST/Solution 3/Find Closest Value In BST.py
@@ -1,24 +1,22 @@
 import math
 def findClosestValueInBst(tree, target):
     # Write your code here.
-    closest_value = math.inf
-    q = [tree]
-    while q:
-        node = q.pop()
-        if min(abs(target - closest_value), abs(target - node.value)) == abs(target - closest_value):
-            closest_value = closest_value
-        else:
-            closest_value = node.value
-        if node.right:
-            q.append(node.right)
-        if node.left:
-            q.append(node.left)
-    
-    return closest_value
+    minValue = math.inf
+    def dfs(tree, minValue):
+        if not tree:
+            return minValue
 
 
+        if abs(target - tree.value) < abs(target - minValue):
+            minValue = tree.value
 
 
+        if target > tree.value:
+            return dfs(tree.right, minValue)
+        elif target < tree.value:
+            return dfs(tree.left, minValue)
+        return minValue
+    return dfs(tree, minValue)
 # This is the class of the input tree. Do not edit.
 class BST:
     def __init__(self, value):
