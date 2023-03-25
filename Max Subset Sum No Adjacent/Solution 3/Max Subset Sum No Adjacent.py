e030c4f49c32ad2914fa2a9815db710aedfe1afd
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-03-25 00:30:58 -0400

    Change Python Solution 3 for question Max Subset Sum No Adjacent

--- Max Subset Sum No Adjacent/Solution 3/Max Subset Sum No Adjacent.py
+++ Max Subset Sum No Adjacent/Solution 3/Max Subset Sum No Adjacent.py
@@ -1,15 +1,13 @@
 def maxSubsetSumNoAdjacent(array):
-    # Write your code here.
-    n = len(array) - 1
-    return helper(n, array)
-def helper(i, array):
     if not array:
         return 0
-    if i == 0:
+    if len(array) == 1:
         return array[0]
-    elif i == 1:
-        return max(array[1], array[0])
-    else:
-        return max(helper(i-1, array), helper(i-2, array)+array[i])
-    
-
+    # Write your code here.
+    n = len(array)
+    ans = [0] * n
+    ans[0] = array[0]
+    ans[1] = max(array[0], array[1])
+    for i in range(2, n):
+        ans[i] = max(ans[i-1], ans[i-2]+array[i])
+    return ans[-1]
