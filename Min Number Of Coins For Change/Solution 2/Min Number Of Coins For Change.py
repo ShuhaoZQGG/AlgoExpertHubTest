5c22414a5875fae74c306750704688fb235e5914
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-03-24 23:32:53 -0400

    Create Python Solution 2 for question Min Number Of Coins For Change

---
+++ Min Number Of Coins For Change/Solution 2/Min Number Of Coins For Change.py
@@ -0,0 +1,9 @@
+def minNumberOfCoinsForChange(n, denoms):
+    # Write your code here.
+    nums = [0] + [float("inf")] * (n)
+    for denom in denoms:
+        for i in range(n + 1):
+            if denom <= i:
+                nums[i] = min(nums[i], 1 + nums[i - denom])
+    return nums[n] if nums[n] !=  float("inf") else -1
+
