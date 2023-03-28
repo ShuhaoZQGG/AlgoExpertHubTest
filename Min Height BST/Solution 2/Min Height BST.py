91f8e45351d512c654fb74c7ec37d15c759deb00
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-03-27 20:54:23 -0400

    Create Python Solution 2 for question Min Height BST

---
+++ Min Height BST/Solution 2/Min Height BST.py
@@ -0,0 +1,35 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+                
+def minHeightBst(array):
+    return formBstTree(array, BST(None))
+    
+def formBstTree(array, tree):
+    n = len(array) // 2
+    if len(array) < 1:
+        return tree
+        
+    if tree.value is None:
+        tree = BST(array[n])
+    else:
+        tree.insert(array[n])
+   
+    formBstTree(array[0:n], tree)
+    formBstTree(array[n+1:len(array) + 1], tree)
+    return tree
