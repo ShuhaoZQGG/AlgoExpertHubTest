9775a078787519068c9cc67acca9dcf9b8391f01
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-06-27 22:43:26 -0400

    Change Python Solution 1 for question Merge Linked Lists

--- Merge Linked Lists/Solution 1/Merge Linked Lists.py
+++ Merge Linked Lists/Solution 1/Merge Linked Lists.py
@@ -12,29 +12,18 @@ def mergeLinkedLists(headOne, headTwo):
     ptr1, ptr2 = headOne, headTwo
     if ptr1.value > ptr2.value:
         ptr1, ptr2 = ptr2, ptr1
-    dummy = ptr1
-    while ptr1.next and ptr2.next:
-        print(ptr1.value)
+    dummy = prevPtr1 = ptr1
+    while ptr1 and ptr2:
         nextPtr1 = ptr1.next
         nextPtr2 = ptr2.next
-        if ptr1.next.value > ptr2.value:
+        if ptr1.next and ptr1.next.value > ptr2.value:
             ptr1.next = ptr2
             ptr2.next = nextPtr1
             ptr2 = nextPtr2
-        else:
-            ptr1 = ptr1.next
-    if ptr2 and not(ptr2.next):
-        while ptr1.next:
-            print(ptr1.next.value)
-            nextPtr1 = ptr1.next
-            if ptr1.next.value > ptr2.value:
-                ptr1.next = ptr2
-                ptr1.next.next = nextPtr1
-                break
-            else:
-                ptr1 = ptr1.next
-    if not(ptr1.next) and ptr2:
-        ptr1.next = ptr2
+        prevPtr1 = ptr1
+        ptr1 = ptr1.next
+    if not(ptr1) and ptr2:
+        prevPtr1.next = ptr2
 
 
     return dummy
