57bfd76f5743f62d4e239510bbfdd9690a59b42a
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2023-08-04 09:32:00 -0400

    Create Python Solution 1 for question Max Profit With K Transactions

---
+++ Max Profit With K Transactions/Solution 1/Max Profit With K Transactions.py
@@ -0,0 +1,13 @@
+def maxProfitWithKTransactions(prices, k):
+    # Write your code here.
+    if len(prices) == 0:
+        return 0
+    matrix = [[0 for i in prices] for j in range(k + 1)]
+    for i in range(1, len(matrix)):
+        localMaxProfit = float("-inf")
+        for j in range(1, len(matrix[0])):
+            localMaxProfit = max(localMaxProfit, matrix[i-1][j-1] - prices[j-1])
+            matrix[i][j] = max(matrix[i][j-1], localMaxProfit + prices[j])
+    return matrix[-1][-1]
+            
+
